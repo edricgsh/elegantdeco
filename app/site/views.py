833fa1302b3fbb6d4404396b33bb876bb72f82ae
Konstantinos Zigogiannis <konstantinoszigogiannis@Konstantinoss-MacBook.local>
2018-06-06 14:35:15 +0300

    ok

--- app/site/views.py
+++ app/site/views.py
@@ -5,3 +5,12 @@ from app.site import site
 @site.route('/')
 def index():
     return render_template('site/index.html', title="Welcome")
+@site.route('/terms')
+def terms():
+    return render_template('site/terms.html', title="Terms")
+@site.route('/returns')
+def returns():
+    return render_template('site/returns.html', title="Returns")
+@site.route('/shipping')
+def shipping():
+    return render_template('site/shipping.html', title="Shipping")
