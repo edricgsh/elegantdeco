cc16cd76f6fbd5c3d08cab0c28e065c1d1a6157d
Konstantinos Zigogiannis <konstantinoszigogiannis@Konstantinoss-MacBook.local>
2018-06-07 16:57:38 +0300

    update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
 
-    SECRET_KEY = 'Dtranq12!pal1kar1@!'
+    SECRET_KEY = 'Dtranq12!pal1kar1@!@'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/konstantinoszigogiannis/Documents/myapps/elegantdeco/db/elegantdeco.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True #for development. Change to false for production
