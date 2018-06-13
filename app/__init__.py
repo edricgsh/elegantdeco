7c976df0e2ec7a74f36b82e3af62ec4014a5ec07
kzigogiannis <kzigogiannis@gmail.com>
2018-06-13 17:40:26 +0300

    final

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,30 @@
 #app/__init.py__
 import os
-
-# third-party imports
-from flask import Flask, render_template
-from flask_migrate import Migrate
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-# local imports
-from instance.config import app_config
+from flask_migrate import Migrate
+from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
-def create_app(config_name):
-
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate = Migrate(app, db)
-
-    from app import models
+    migrate.init_app(app, db)
 
 
 
     from .site import site as site_blueprint
     app.register_blueprint(site_blueprint)
 
-
     return app
+
+
+
+
+
+from app import models
--- run.py
+++ run.py
@@ -1,11 +1,5 @@
 # run.py
 
-import os
+from app import create_app, db
 
-from app import create_app
-
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
-
-if __name__ == '__main__':
-    app.run()
+app = create_app()
