4a9aec24d4a5c8797d1cdbc80635dd50555f154f
kzigogiannis <kzigogiannis@gmail.com>
2018-06-13 15:52:53 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -7,22 +7,16 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 # local imports
-from config import app_config
+from instance.config import app_config
 
 db = SQLAlchemy()
 
 
 def create_app(config_name):
-    if os.getenv('FLASK_CONFIG') == "production":
-        app = Flask(__name__)
-        app.config.update(
-            SECRET_KEY=os.getenv('SECRET_KEY'),
-            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
-        )
-    else:
-        app = Flask(__name__, instance_relative_config=True)
-        app.config.from_object(app_config[config_name])
-        app.config.from_pyfile('config.py')
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
 
     db.init_app(app)
     migrate = Migrate(app, db)
